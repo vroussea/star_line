7a76b8b61ed3de884e66caff1111e2ac953776be
Raokk <59ichiigo@gmail.com>
2022-03-02 16:42:17 +0100

    fix cargo toml

--- lib.rs
+++ lib.rs
@@ -1,9 +1,6 @@
 pub mod functions;
 
 pub fn run() -> Result<(), functions::errors::CustomError> {
-    let mut map: functions::map::Map = functions::parser::parse()?;
-    map.resolve();
-    println!("{}", map);
 
     return Ok(());
 }
